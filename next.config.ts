d45ea9a87454777640ca6c6b77d5a9a7c3b5e05f
James <jtclarkjr@ucla.edu>
2025-09-15 21:08:21 +0900

    fix: next config for images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com' // Google avatars
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com' // GitHub avatars
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com' // Discord avatars
+      }
+    ]
+  }
+}
 
 export default nextConfig
