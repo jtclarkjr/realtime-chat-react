4a27707ce2226053ae3483283d7badcf47967c58
J <jtclarkjr@ucla.edu>
2025-09-24 18:20:52 +0900

    fix: border limited to desktop viewport

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -43,7 +43,7 @@ export default async function RoomPage({ params }: RoomPageProps) {
   }
 
   return (
-    <section className="border-l border-r bg-background md:max-w-5xl md:mx-auto">
+    <section className="md:border-l md:border-r bg-background md:max-w-5xl md:mx-auto">
       <RoomClient room={room} initialMessages={messages} user={user} />
     </section>
   )
