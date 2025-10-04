63e2917dae67f99798135718e21e0c4d56b468c1
J <jtclarkjr@ucla.edu>
2025-10-05 00:53:51 +0900

    chore: remove google next image pattern

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com' // Google avatars
-      },
-      {
-        protocol: 'https',
         hostname: 'avatars.githubusercontent.com' // GitHub avatars
       },
       {
