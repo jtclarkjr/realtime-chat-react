d615886c3030973faa3110e0b38d8e5d4761e010
J <jtclarkjr@ucla.edu>
2025-10-09 13:43:57 +0900

    fix: global types

---
+++ global.d.ts
@@ -0,0 +1,26 @@
+// Global type declarations for side-effect imports
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.styl' {
+  const content: Record<string, string>;
+  export default content;
+}
