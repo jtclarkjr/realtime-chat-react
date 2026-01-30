a330f2b89fb761dd52c374a00ac1b77d4a6f6a02
J <jtclarkjr@ucla.edu>
2026-01-30 10:18:05 +0900

    chore: move realtime times numbers to constants

---
+++ lib/supabase/constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Realtime connection configuration constants
+ */
+export const REALTIME_EVENTS_PER_SECOND = 10
+export const REALTIME_HEARTBEAT_INTERVAL_MS = 30000
+export const REALTIME_CONNECTION_TIMEOUT_MS = 20000
+export const REALTIME_INITIAL_BACKOFF_MS = 1000
+export const REALTIME_MAX_BACKOFF_MS = 10000
+export const REALTIME_MAX_BACKOFF_TRIES = 4
